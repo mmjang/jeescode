2a144acd799720549a148e06092b4ae94b8a37b4
mmjang <752515918@qq.com>
2021-08-01 15:16:46 +0800

    feature: use gittalk

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import logo from "./logo.svg";
 import "./App.css";
+import "gitalk/dist/gitalk.css";
 import Sandbox from "./components/Sandbox";
 import Banner from "./components/Banner";
+import useQuestionList from "./hooks/useQuestionList";
+import { useState } from "react";
+import useQuestionNumber from "./hooks/useQuestionNumber";
 
 function App() {
+  const questionList = useQuestionList();
+  const questionNumber = useQuestionNumber();
+  const [selected, setSelected] = useState(1);
+
+  function onSelectionChange(event) {
+    window.location.href = `#${event.target.value}`;
+    debugger;
+    setSelected(event.target.value);
+  }
+
   return (
     <div className="w-screen h-screen flex flex-col">
-      <Banner></Banner>
-      <Sandbox></Sandbox>
+      <Banner>
+        <span className="pl-4 p-1 text-blue-700">题目列表：</span>
+        <select value={selected} onChange={onSelectionChange}>
+          {questionList.map((q) => (
+            <option value={q.questionNumber}>
+              {`${q.questionNumber}. ${q.title}`}
+            </option>
+          ))}
+        </select>
+      </Banner>
+      <Sandbox questionNumber={questionNumber}></Sandbox>
     </div>
   );
 }
